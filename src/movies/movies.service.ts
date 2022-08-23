e5e81969bc8155ebb8a0ef794448b72fd38df94d
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2022-08-23 13:56:38 +0900

    Update movies.service.ts

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateMovieDto } from './dto/create-movie.dot';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
